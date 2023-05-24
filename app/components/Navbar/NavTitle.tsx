0c84454b490501ee752713783cb7f22c3953fe4e
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-05-24 11:14:05 -0400

    extract title

---
+++ app/components/Navbar/NavTitle.tsx
@@ -0,0 +1,21 @@
+import { Typography } from '@mui/material';
+import Link from 'next/link';
+
+const NavTitle = () => {
+  return (
+    <Typography
+      variant='h5'
+      component='h1'
+      noWrap
+      sx={{
+        fontSize: { xs: '1.5rem', md: '1.75rem' },
+        width: 'max-content',
+        fontWeight: 700,
+        letterSpacing: '.2rem'
+      }}
+    >
+      <Link href='/'>DAVID GOEHRING</Link>
+    </Typography>
+  );
+};
+export default NavTitle;
