8698afd42294744ae9dbadd8000982027a2a5928
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-05-24 14:42:02 -0400

    rename nav menu to drawer

---
+++ app/hooks/useDrawer.ts
@@ -0,0 +1,14 @@
+import { useState, MouseEvent } from 'react';
+
+export const useDrawer = () => {
+  const [isDrawerOpen, toggleIsDrawerOpen] = useState<boolean>(false);
+
+  const toggleDrawer = (event: MouseEvent<HTMLElement>) => {
+    toggleIsDrawerOpen(!isDrawerOpen);
+  };
+
+  return {
+    isDrawerOpen,
+    toggleDrawer
+  };
+};
