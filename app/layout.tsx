5681017010afee51562c01617af8ef06cf514ac4
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-05-24 14:37:24 -0400

    move nav and theme context to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,38 @@
+'use client';
+import './globals.css';
+import { ThemeProvider } from '@mui/material/styles';
+import { ThemeContext } from './contexts/themeContext';
+import { useThemeToggle } from './hooks/useThemeToggle';
+import { getTheme } from './theme';
+import CssBaseline from '@mui/material/CssBaseline';
 import { Inter } from 'next/font/google';
+import Navbar from './components/Navbar/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'David Goehring',
-  description: 'Los Angeles Video Editor'
-};
+// export const metadata = {
+//   title: 'David Goehring',
+//   description: 'Los Angeles Video Editor'
+// };
 
 export default function RootLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
+  const [mode, toggleTheme] = useThemeToggle();
+  const theme = getTheme(mode);
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeContext.Provider value={{ toggleTheme }}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Navbar />
+            {children}
+          </ThemeProvider>
+        </ThemeContext.Provider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,8 @@
-'use client';
-import './globals.css';
-import { ThemeProvider } from '@mui/material/styles';
-import { ThemeContext } from './contexts/themeContext';
-import { useThemeToggle } from './hooks/useThemeToggle';
-import { getTheme } from './theme';
-import CssBaseline from '@mui/material/CssBaseline';
-import Navbar from './components/Navbar/Navbar';
-
 const Page = () => {
-  const [mode, toggleTheme] = useThemeToggle();
-  const theme = getTheme(mode);
-
   return (
-    <ThemeContext.Provider value={{ toggleTheme }}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Navbar />
-      </ThemeProvider>
-    </ThemeContext.Provider>
+    <>
+      <h1>home</h1>
+    </>
   );
 };
 
