eaaa13c54682985df6cf84efacca30ad8b7775ae
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-01 15:33:24 -0400

    render thumbnails on cards

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,17 @@ const Page = async () => {
   const videoData = fetchVideos();
   const { data } = await Promise.resolve(videoData);
 
-  return <VideoGallery videos={data} />;
+  const videosWithThumbnails: VideoWithThumbnail[] = data.map((video: any) => {
+    video.name = video.name.replace(/^\d+\s/, '');
+    video.thumbnail = {
+      src: video.pictures.sizes[video.pictures.sizes.length - 1].link,
+      height: video.pictures.sizes[3].height,
+      width: video.pictures.sizes[3].width,
+    };
+    return video;
+  });
+
+  return <VideoGallery videos={videosWithThumbnails} />;
 };
 
 export default Page;
