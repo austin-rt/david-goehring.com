26109c25a22955a77f21b9182ee43fbb63ebab96
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-02 15:11:51 -0400

    father why have you forsaken me

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,3 +4,8 @@ export const NAVIGATION: Navigation = {
   ABOUT: 'about',
   CONTACT: 'contact',
 };
+
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : 'http://localhost:3000';
