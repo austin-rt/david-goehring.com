363e100ca9caba9de50f2bd68f1bd2c48aa785cf
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-02 12:10:37 -0400

    update api base

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import VideoGallery from './components/VideoGallery/VideoGallery';
 
 async function getData() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/api/vimeo`);
+  const res = await fetch(
+    `${
+      process.env.NEXT_PUBLIC_VERCEL_ENV || 'http://localhost:3000'
+    }/api/vimeo`,
+  );
   if (!res.ok) {
     throw new Error('Failed to fetch data');
   }
