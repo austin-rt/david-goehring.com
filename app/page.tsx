c0afe753b06d939a54f94beb364d08dc6394e7d5
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-02 11:05:04 -0400

    rm hardcode localhost

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import VideoGallery from "./components/VideoGallery/VideoGallery";
+import VideoGallery from './components/VideoGallery/VideoGallery';
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/vimeo");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_ENV}/api/vimeo`);
   if (!res.ok) {
-    throw new Error("Failed to fetch data");
+    throw new Error('Failed to fetch data');
   }
 
   return res.json();
