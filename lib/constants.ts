6a1a325662a01747b50cdc6f651782e7030dadad
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-02 14:48:07 -0400

    DUDE FFS

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,4 +5,7 @@ export const NAVIGATION: Navigation = {
   CONTACT: 'contact',
 };
 
-export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000';
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+    ? `https://www.${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : 'http://localhost:3000';
