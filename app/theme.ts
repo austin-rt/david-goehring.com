b827dd6d4b7197c6f7530c48edf69c74235d3503
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-06-03 13:40:51 -0400

    restyle theme buttons and form

--- app/theme.ts
+++ app/theme.ts
@@ -12,10 +12,59 @@ export const getTheme = (mode: 'light' | 'dark') =>
       MuiButton: {
         styleOverrides: {
           root: ({ ownerState }) => ({
+            // nav links
             ...(ownerState.variant === 'text' &&
               ownerState.color === 'primary' &&
               mode === 'light' && {
                 color: colors.grey[900],
+              }),
+            // form buttons light mode
+            ...(ownerState.variant === 'outlined' &&
+              ownerState.color === 'primary' &&
+              mode === 'light' && {
+                color: colors.grey[600],
+                borderColor: colors.grey[600],
+                '&:hover': {
+                  color: colors.grey[800],
+                  borderColor: colors.grey[900],
+                },
+              }),
+            // form buttons dark mode
+            ...(ownerState.variant === 'outlined' &&
+              ownerState.color === 'primary' &&
+              mode === 'dark' && {
+                color: colors.grey[400],
+                '&:hover': {
+                  backgroundColor: 'transparent',
+                  color: colors.grey[100],
+                },
+              }),
+          }),
+        },
+      },
+      MuiTextField: {
+        styleOverrides: {
+          root: ({ ownerState }) => ({
+            ...(ownerState.variant === 'outlined' &&
+              ownerState.color === 'primary' &&
+              mode === 'light' && {
+                '& .MuiOutlinedInput-root': {
+                  '&:hover fieldset': {
+                    border: `solid 1px ${colors.grey[600]}`,
+                  },
+                  '&.Mui-focused fieldset': {
+                    border: `solid 1px ${colors.grey[600]}`,
+                  },
+                  '& Mui-focused': {
+                    color: colors.grey[600],
+                  },
+                },
+                '& .MuiInputLabel-root': {
+                  color: colors.grey[600],
+                },
+                '& .MuiInputLabel-root.Mui-focused': {
+                  color: colors.grey[600],
+                },
               }),
           }),
         },
@@ -23,6 +72,5 @@ export const getTheme = (mode: 'light' | 'dark') =>
     },
   });
 
-// button colors reverse
 // text field floating label color reverse
 // grid items aspect ration 1:1 grid/card/paper ?
